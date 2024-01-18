817782deb146b5d34716f3ca0cf743ed335e2974
Jose BM <jjbaxin@sice.com>
2024-01-18 17:17:38 -0600

    Se inicia con el widget para ver las películas en cartas que se pueden mover utilizando el paquete Card_swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
-import 'package:cinemapedia/config/constants/environment.dart';
-import 'package:cinemapedia/presentation/screens/providers/providers.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -47,15 +47,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   if ( nowPlayingMovies.length == 0 ) return CircularProgressIndicator();
 
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index){
-        final movie  = nowPlayingMovies[index];
-
-        return ListTile(
-          title: Text( movie.title ),
-        );
-
-    });
+    return Column(
+      children: [
+        const CustomAppBar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ],
+    );
   }
 }
